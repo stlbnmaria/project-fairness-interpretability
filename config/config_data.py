9e00804e154b855601e4b6595609cb61f97e0797
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-27 21:14:48 +0200

    created config folder and intit config for data preprocessing

---
+++ config/config_data.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DATA_PATH = Path("data") / "file65ef3a759daf.arff"
+DROP_COLS = ["Description", "Model", "Charge", "Driver.City", "Arrest.Type"]
+OUT_PATH = Path("data") / "data.csv"
