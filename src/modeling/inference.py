d0d98af9470991f871663d54f80afca6bedc00d2
Joao Melo <joaohfpmelo@gmail.com>
2023-10-29 17:50:11 +0000

    Added inference function to inference.py

--- src/modeling/inference.py
+++ src/modeling/inference.py
@@ -1,2 +1,38 @@
-def inference() -> None:
-    pass
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from pygam import LogisticGAM
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+
+
+def inference(
+    model: Union[
+        RandomForestClassifier, LogisticRegression, DecisionTreeClassifier, LogisticGAM
+    ] = None,
+    X: pd.DataFrame = None,
+    predict_proba: bool = False,
+) -> np.ndarray:
+    """Given X and a model, returns an array containing the predictions(probabilities or labels).
+
+    Parameters
+    ----------
+    model: Union[RandomForestClassifier, LogisticRegression, DecisionTreeClassifier, LogisticGAM]
+        Model which will be used to make the predictions.
+    X: pd.DataFrame
+        Data for which the model will predict the target variable label or probability.
+    predict_proba: bool
+        Boolean which represents whether the output should be the predicted labels or probabilities.
+
+    Returns
+    -------
+    preds: np. ndarray
+        Array containing the predicted labels/probabilities.
+    """
+    if predict_proba:
+        preds = model.predict_proba(X)
+    else:
+        preds = model.predict(X)
+    return preds
