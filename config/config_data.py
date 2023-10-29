fc09ca43188fd4b1d91bb0dc552c15469382aba3
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-29 09:59:15 +0100

    correct paths to yaml configs

--- config/config_data.py
+++ config/config_data.py
@@ -3,6 +3,6 @@ from pathlib import Path
 DATA_PATH = Path("data") / "file65ef3a759daf.arff"
 DROP_COLS = ["Description", "Model", "Charge", "Driver.City", "Arrest.Type"]
 OUT_PATH = Path("data") / "data.csv"
-M_DICT_PATH = "../../config/make_dict.yaml"
-M_DICT_H_PATH = "../../config/make_dict_hard.yaml"
+M_DICT_PATH = Path("config") / "make_dict.yaml"
+M_DICT_H_PATH = Path("config") / "make_dict_hard.yaml"
 N_CATEGORIES = 20
