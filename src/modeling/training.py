1535ef17fdeae0d8e630ffc91274c5cb81da9b9c
Joao Melo <joaohfpmelo@gmail.com>
2023-10-29 17:23:39 +0000

    Added training function to training.py and andded GAM to model selections.Added pygam to requirements.in

--- src/modeling/training.py
+++ src/modeling/training.py
@@ -1,24 +1,58 @@
 from typing import Union
 
+import pandas as pd
+from pygam import LogisticGAM
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 
-# TODO: integrate tracking with mlflow
-
 
 def get_model(
     model_name: str,
-) -> Union[RandomForestClassifier, LogisticRegression, DecisionTreeClassifier]:
+) -> Union[RandomForestClassifier, LogisticRegression, DecisionTreeClassifier, LogisticGAM]:
+    """Creates an ML model based on the model given as string.
+
+    Parameters
+    ----------
+    model_name: str
+            Name of the model we want to create.
+
+    Returns
+    -------
+    model: Union[RandomForestClassifier, LogisticRegression, DecisionTreeClassifier, LogisticGAM]
+        ML model we intended to create.
+    """
     if model_name == "Logistic Regression":
         model = LogisticRegression()
     elif model_name == "Decision Tree":
         model = DecisionTreeClassifier()
     elif model_name == "Random Forest":
         model = RandomForestClassifier()
+    elif model_name == "GAM":
+        model = LogisticGAM()
 
     return model
 
 
-def training() -> None:
-    pass
+def training(
+    model_name: str, X_train: pd.DataFrame, y_train: pd.Series
+) -> Union[RandomForestClassifier, LogisticRegression, DecisionTreeClassifier, LogisticGAM]:
+    """Given the name of the ML model we want, creats the model, and fits it to X_train and Y_train.
+
+    Parameters
+    ----------
+    model_name: str
+        Name of the model we want to create.
+    X_train: pd.DataFrame
+        Pandas dataframe containing the training data without the target variable.
+    y_train: pd.Series
+        Pandas Series containing the values of the target variable for the training data.
+
+    Returns
+    -------
+    model: Union[RandomForestClassifier, LogisticRegression, DecisionTreeClassifier, LogisticGAM]
+        Model we wanted to create, fitted to X_train and y_train.
+    """
+    model = get_model(model_name)
+    model.fit(X_train, y_train)
+    return model
