5b6af114b4d174060e6e3436572dcd54af4ba238
DataTom <sch.thomas@live.de>
2023-10-30 18:00:26 +0100

    adding unique run name to each model that is run

--- config/config_modeling.py
+++ config/config_modeling.py
@@ -6,17 +6,33 @@ RANDOM_STATE = 42
 
 # modeling
 EXPERIMENT = "Final Models"
-RUN_NAME = "TEST ALL - Base"
 MODELS_TO_COMPARE = [
     {
+        "RUN_NAME": "Random Forest - Base",
         "MODEL_NAME": "Random Forest",
         "PARAMS": {"max_depth": 50, "ccp_alpha": 0.001, "max_samples": 0.8},
     },
-    {"MODEL_NAME": "Logistic Regression", "PARAMS": {"C": 1.0, "penalty": "l2", "solver": "lbfgs"}},
-    {"MODEL_NAME": "Decision Tree", "PARAMS": {"max_depth": 20, "min_samples_split": 2}},
-    {"MODEL_NAME": "GAM", "PARAMS": {"max_iter": 100, "tol": 0.0001}},
-    {"MODEL_NAME": "XGB", "PARAMS": {"max_depth": 20, "n_estimators": 100}},
-    {"MODEL_NAME": "ANN", "PARAMS": {"hidden_layer_sizes": (100,), "activation": "relu"}},
+    {
+        "RUN_NAME": "Logistic Regression - Base",
+        "MODEL_NAME": "Logistic Regression",
+        "PARAMS": {"C": 1.0, "penalty": "l2", "solver": "lbfgs"},
+    },
+    {
+        "RUN_NAME": "Decision Tree - Base",
+        "MODEL_NAME": "Decision Tree",
+        "PARAMS": {"max_depth": 20, "min_samples_split": 2},
+    },
+    {"RUN_NAME": "GAM - Base", "MODEL_NAME": "GAM", "PARAMS": {"max_iter": 100, "tol": 0.0001}},
+    {
+        "RUN_NAME": "XGB - Base",
+        "MODEL_NAME": "XGB",
+        "PARAMS": {"max_depth": 20, "n_estimators": 100},
+    },
+    {
+        "RUN_NAME": "ANN - Base",
+        "MODEL_NAME": "ANN",
+        "PARAMS": {"hidden_layer_sizes": (100,), "activation": "relu"},
+    },
 ]
 
 # inference
