21b787ffd215b318905e458ed92bbc001e258f31
DataTom <sch.thomas@live.de>
2023-10-30 16:21:44 +0100

    adding other models to config file

--- config/config_modeling.py
+++ config/config_modeling.py
@@ -6,9 +6,23 @@ RANDOM_STATE = 42
 
 # modeling
 EXPERIMENT = "Final Models"
-RUN_NAME = "Random Forest - Base"
-MODEL_NAME = "Random Forest"
-PARAMS = {"max_depth": 50, "ccp_alpha": 0.001, "max_samples": 0.8}
+RUN_NAME = "TEST ALL - Base"
+# MODEL_NAME = "Random Forest"
+# PARAMS = {"max_depth": 50, "ccp_alpha": 0.001, "max_samples": 0.8}
+MODELS_TO_COMPARE = [
+    {
+        "MODEL_NAME": "Random Forest",
+        "PARAMS": {"max_depth": 50, "ccp_alpha": 0.001, "max_samples": 0.8},
+    },
+    {"MODEL_NAME": "Logistic Regression", "PARAMS": {"C": 1.0, "penalty": "l2", "solver": "lbfgs"}},
+    {"MODEL_NAME": "Decision Tree", "PARAMS": {"max_depth": 20, "min_samples_split": 2}},
+    # {
+    #     "MODEL_NAME": "GAM",
+    #     "PARAMS": {"max_iter": 100, "tol": 0.0001}
+    # },
+    {"MODEL_NAME": "XGB", "PARAMS": {"max_depth": 20, "n_estimators": 100}},
+    {"MODEL_NAME": "ANN", "PARAMS": {"hidden_layer_sizes": (100,), "activation": "relu"}},
+]
 
 # inference
 THRESHOLD = 0.5
