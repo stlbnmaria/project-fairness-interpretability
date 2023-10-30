0abb22e0c59ee3429dfa6579f002da5c7ffaa734
Maria Stoelben <maria.stlbn@hotmail.de>
2023-10-30 19:44:16 +0100

    implemented grid search

--- config/config_modeling.py
+++ config/config_modeling.py
@@ -5,33 +5,46 @@ TEST_FROM_VAL = 0.5
 RANDOM_STATE = 42
 
 # modeling
-EXPERIMENT = "Final Models"
+EXPERIMENT = "Tuning"
 MODELS_TO_COMPARE = [
     {
-        "RUN_NAME": "Random Forest - Base",
+        "RUN_NAME": "Tuning Run 1.",
         "MODEL_NAME": "Random Forest",
-        "PARAMS": {"max_depth": 50, "ccp_alpha": 0.001, "max_samples": 0.8},
+        "PARAMS": {"max_depth": [30, 50], "ccp_alpha": [0.0001, 0.001], "max_samples": [0.7, 0.8]},
     },
     {
-        "RUN_NAME": "Logistic Regression - Base",
+        "RUN_NAME": "Tuning Run 1.",
         "MODEL_NAME": "Logistic Regression",
-        "PARAMS": {"C": 1.0, "penalty": "l2", "solver": "lbfgs"},
+        "PARAMS": {"C": [0.9, 1.0], "penalty": ["l2"], "solver": ["lbfgs"]},
     },
     {
-        "RUN_NAME": "Decision Tree - Base",
+        "RUN_NAME": "Tuning Run 1.",
         "MODEL_NAME": "Decision Tree",
-        "PARAMS": {"max_depth": 20, "min_samples_split": 2},
+        "PARAMS": {"max_depth": [10, 20], "min_samples_split": [2]},
     },
-    {"RUN_NAME": "GAM - Base", "MODEL_NAME": "GAM", "PARAMS": {"max_iter": 100, "tol": 0.0001}},
     {
-        "RUN_NAME": "XGB - Base",
+        "RUN_NAME": "Tuning Run 1.",
+        "MODEL_NAME": "GAM",
+        "PARAMS": {"max_iter": [100], "tol": [0.0001]},
+    },
+    {
+        "RUN_NAME": "Tuning Run 1.",
         "MODEL_NAME": "XGB",
-        "PARAMS": {"max_depth": 20, "n_estimators": 100},
+        "PARAMS": {"max_depth": [20, 30], "n_estimators": [100]},
     },
     {
-        "RUN_NAME": "ANN - Base",
+        "RUN_NAME": "Tuning Run 1.",
         "MODEL_NAME": "ANN",
-        "PARAMS": {"hidden_layer_sizes": (100,), "activation": "relu"},
+        "PARAMS": {
+            "hidden_layer_sizes": [
+                (100,),
+                (
+                    80,
+                    40,
+                ),
+            ],
+            "activation": ["relu"],
+        },
     },
 ]
 
--- src/modeling/main.py
+++ src/modeling/main.py
@@ -6,6 +6,7 @@ import mlflow
 from create_data_split import split_data
 from evaluation import eval_metrics
 from inference import inference
+from sklearn.model_selection import ParameterGrid
 from training import training
 
 from config.config_data import OUT_PATH
@@ -29,38 +30,56 @@ def main(
     test_size: float,
     random_state: int,
     model_name: str,
-    params: dict[str, Union[float, int, bool, Tuple[int]]],
+    params: dict[str, Union[str, float, int, bool, Tuple[int]]],
     threshold: float,
 ) -> None:
     # validate that mlflow runs locally
     print(f"tracking URI: '{mlflow.get_tracking_uri()}'")
 
+    # create param grid based on all combinations
+    param_grid = ParameterGrid(params)
+
+    # make unique experiment per model and run increasing sequentially
+    if "Tuning" in experiment:
+        experiment += f" - {model_name}"
+        run_name += ".1"
+
     # set experiment
     mlflow.set_experiment(experiment)
 
-    with mlflow.start_run(run_name=run_name):
-        # dummy encoding and data split
-        data = split_data(
-            cols=cat_cols,
-            data_path=data_path,
-            train_size=train_size,
-            test_size=test_size,
-            random_state=random_state,
-        )
+    # iterate over param grid, create mlflow run and perform training, val, test
+    i = 1
+    for params in param_grid:
+        if (i != 1) & ("Tuning" in experiment):
+            run_name = run_name.replace("." + str(i - 1), "." + str(i))
 
-        # training
-        model = training(
-            model_name=model_name, X_train=data["train"][0], y_train=data["train"][1], params=params
-        )
-        mlflow.log_params(params)
+        with mlflow.start_run(run_name=run_name):
+            # dummy encoding and data split
+            data = split_data(
+                cols=cat_cols,
+                data_path=data_path,
+                train_size=train_size,
+                test_size=test_size,
+                random_state=random_state,
+            )
 
-        # predictions and evalutation
-        for state in ["train", "val", "test"]:
-            preds_prob = inference(model=model, X=data[state][0])
-            preds = preds_prob > threshold
-            metrics = eval_metrics(data[state][1], preds, preds_prob)
-            metrics = {state + "_" + k: v for k, v in metrics.items()}
-            mlflow.log_metrics(metrics)
+            # training
+            model = training(
+                model_name=model_name,
+                X_train=data["train"][0],
+                y_train=data["train"][1],
+                params=params,
+            )
+            mlflow.log_params(params)
+
+            # predictions and evalutation
+            for state in ["train", "val", "test"]:
+                preds_prob = inference(model=model, X=data[state][0])
+                preds = preds_prob > threshold
+                metrics = eval_metrics(data[state][1], preds, preds_prob)
+                metrics = {state + "_" + k: v for k, v in metrics.items()}
+                mlflow.log_metrics(metrics)
+        i += 1
 
 
 if __name__ == "__main__":
