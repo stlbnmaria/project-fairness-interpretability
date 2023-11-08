857c21fd7bb4ac6b9b9ec9c445fe2b8080d67496
DataTom <sch.thomas@live.de>
2023-11-08 01:04:13 +0100

    changed number of topics to extract using NLP to 4

--- config/config_data.py
+++ config/config_data.py
@@ -1,9 +1,17 @@
 from pathlib import Path
 
 DATA_PATH = Path("data") / "file65ef3a759daf.arff"
-DROP_COLS = ["Description", "Model", "Charge", "Driver.City", "Arrest.Type", "Commercial.Vehicle"]
+DROP_COLS = [
+    "Description",
+    "Model",
+    "Charge",
+    "Driver.City",
+    "Arrest.Type",
+    "Commercial.Vehicle",
+    "description_clean",
+]
 OUT_PATH = Path("data") / "data.csv"
 DICT_PATH = Path("config") / "dict.yaml"
 DICT_H_PATH = Path("config") / "dict_hard.yaml"
 N_CATEGORIES = 15
-N_TOPICS = 10
+N_TOPICS = 4
