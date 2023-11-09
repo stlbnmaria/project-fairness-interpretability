cd44661ec8a458033f5732e7d5a10d8cbe16591a
Maria Stoelben <maria.stlbn@hotmail.de>
2023-11-09 22:28:39 +0100

    best tuned models for thresholded data

--- config/config_modeling.py
+++ config/config_modeling.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 # data processing
 CAT_COLS = ["Color", "Gender", "Make", "Race", "VehicleType"]
 TRAIN_SIZE = 0.6
@@ -5,88 +7,64 @@ TEST_FROM_VAL = 0.5
 RANDOM_STATE = 42
 
 # modeling
-EXPERIMENT = "Tuning - no-plain"
-MODELS_PATH = None
+EXPERIMENT = "Final Models"
+MODELS_PATH = Path("models/")
 MODELS_TO_COMPARE = [
     {
-        "RUN_NAME": "Tuning Run 1.",
+        "RUN_NAME": "Random Forest - threshold",
         "MODEL_NAME": "Random Forest",
         "PARAMS": {
-            "max_depth": [10, 20, 30, 40, 50],
-            "min_samples_leaf": [5, 10, 20, 100],
-            "max_features": ["log2", "sqrt"],
-            "ccp_alpha": [0.0001, 0.005, 0.001],
-            "max_samples": [0.7, 0.8, 0.9, 1],
+            "max_depth": [40],
+            "min_samples_leaf": [5],
+            "max_features": ["sqrt"],
+            "ccp_alpha": [0.0001],
+            "max_samples": [0.8],
         },
     },
     {
-        "RUN_NAME": "Tuning Run 1.",
+        "RUN_NAME": "Logistic Regression - threshold",
         "MODEL_NAME": "Logistic Regression",
         "PARAMS": [
             {
-                "C": [0.8, 0.9, 1.0, 1.1],
+                "C": [0.8],
                 "penalty": ["l2"],
-                "solver": ["lbfgs", "newton-cholesky"],
-            },
-            {
-                "C": [0.8, 0.9, 1.0, 1.1],
-                "penalty": ["l2", "l1"],
-                "solver": ["saga"],
-            },
-            {
-                "C": [0.8, 0.9, 1.0, 1.1],
-                "penalty": ["elasticnet"],
-                "solver": ["saga"],
-                "l1_ratio": [0.3, 0.5, 0.7],
+                "solver": ["newton-cholesky"],
             },
         ],
     },
     {
-        "RUN_NAME": "Tuning Run 1.",
+        "RUN_NAME": "Decision Tree - threshold",
         "MODEL_NAME": "Decision Tree",
         "PARAMS": {
-            "max_depth": [5, 10, 15, 20],
-            "min_samples_split": [10, 20, 30, 40, 50],
-            "ccp_alpha": [0.0001, 0.005, 0.001],
-            "max_features": ["log2", "sqrt", 1],
+            "max_depth": [10],
+            "min_samples_split": [50],
+            "ccp_alpha": [0.0001],
+            "max_features": ["sqrt"],
         },
     },
     {
-        "RUN_NAME": "Tuning Run 1.",
+        "RUN_NAME": "GAM - threshold",
         "MODEL_NAME": "GAM",
-        "PARAMS": {"max_iter": [50, 100, 150], "tol": [0.001, 0.005, 0.0001]},
+        "PARAMS": {"max_iter": [150], "tol": [0.005]},
     },
     {
-        "RUN_NAME": "Tuning Run 1.",
+        "RUN_NAME": "XGB - threshold",
         "MODEL_NAME": "XGB",
         "PARAMS": {
-            "max_depth": [6, 8, 10],
-            "n_estimators": [100, 200, 300],
-            "eta": [0.1, 0.2, 0.3],
-            "subsample": [0.7, 0.8, 0.9],
-            "colsample_bytree": [0.4, 0.6, 0.8, 1],
-            "alpha": [0, 1],
+            "max_depth": [6],
+            "n_estimators": [100],
+            "eta": [0.1],
+            "subsample": [0.9],
+            "colsample_bytree": [0.8],
+            "alpha": [1],
         },
     },
     {
-        "RUN_NAME": "Tuning Run 1.",
+        "RUN_NAME": "ANN - threshold",
         "MODEL_NAME": "ANN",
         "PARAMS": {
-            "hidden_layer_sizes": [
-                (100,),
-                (
-                    120,
-                    60,
-                    30,
-                ),
-                (
-                    120,
-                    60,
-                    30,
-                    15,
-                ),
-            ],
-            "solver": ["lbfgs", "sgd", "adam"],
+            "hidden_layer_sizes": [(100,)],
+            "solver": ["adam"],
         },
     },
 ]
